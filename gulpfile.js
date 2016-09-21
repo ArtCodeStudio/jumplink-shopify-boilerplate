54f3ea80dee173a76885b64739535cd1be45de6a
Pascal Garber <pascal@jumplink.eu>
2016-09-21 22:25:08 +0200

    generate theme settings schema of bootstrap variables

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,10 @@ var gulp          = require('gulp'),
     pjson         = require('./package.json'),
     zip           = require('gulp-zip'),
     SassImport    = require('./utils/sass_import.js'),
-    sass          = require('gulp-sass');
+    sass          = require('gulp-sass'),
+    jsoncombine   = require('gulp-jsoncombine'),
+    fs            = require('fs'),
+    util          = require('util');
 
 // Basic error messages output to the console.
 // Used with plumber so we don't stop the other tasks from running or kill the gulp process on an error
@@ -44,7 +47,7 @@ gulp.task('build', ['sass', 'zip']);
 
 // build sass to test build
 gulp.task('bootstrap_sass_test_build', function () {
-  return gulp.src('./bower_components/bootstrap/scss/bootstrap.scss')
+  return gulp.src('./bower_components/bootstrap-backward/scss/bootstrap-backward.scss')
     .pipe(sass().on('error', onError))
     .pipe(gulp.dest('./css'));
 });
@@ -76,4 +79,118 @@ gulp.task('zip', function () {
   return gulp.src(theme, {base: "."})
     .pipe(zip('JumpLink-Boilerplate-' + pjson.version + '.zip'))
     .pipe(gulp.dest('./'));
+});
+
+// BOOTSTRAP_THEME_SETTINGS: Create bootstrap variables for the settings_schema.json
+gulp.task('bootstrap_theme_settings', function () {
+
+  var bsPkg = require('./bower_components/bootstrap-backward/package.json');
+  var bootstrap_variables = require('./bower_components/bootstrap-backward/variables.json');
+  var bootstrap_theme_settings = {
+    "name": "Bootstrap "+bsPkg.version,
+    "settings": [
+      {
+        "type": "paragraph",
+        "content": "Bootstrap 4 [fork](https:\/\/github.com\/JumpLinkNetwork\/bootstrap-backward) with backward compatibility for Shopify by [JumpLink](https:\/\/www.jumplink.eu)"
+      }
+    ]
+  };
+
+  var groups = {};
+  
+  // sort variables to groups
+  bootstrap_variables.forEach(function(variableDefs) {
+    console.log("variableDefs",variableDefs);
+    if(!groups[variableDefs.group[0]]) {
+      groups[variableDefs.group[0]] = [];
+    }
+    if(variableDefs.type) {
+      variableDefs.type = variableDefs.type.toLowerCase();
+    }
+
+    switch (variableDefs.type) {
+      case 'color':
+        groups[variableDefs.group[0]].push({
+          type: "color",
+          id: 'bs4-'+variableDefs.context.name,
+          label: variableDefs.context.name,
+          default: variableDefs.context.value,
+          info: variableDefs.description
+
+        });
+        break;
+      case 'number':
+        groups[variableDefs.group[0]].push({
+          type: "number",
+          id: 'bs4-'+variableDefs.context.name,
+          label: variableDefs.context.name,
+          default: Number(variableDefs.context.value),
+          info: variableDefs.description
+
+        });
+        break;
+      case 'bool':
+      case 'boolean':
+        groups[variableDefs.group[0]].push({
+          type: "checkbox",
+          id: 'bs4-'+variableDefs.context.name,
+          label: variableDefs.context.name,
+          default: Boolean(variableDefs.context.value),
+          info: variableDefs.description
+
+        });
+        break;
+      default:
+        groups[variableDefs.group[0]].push({
+          type: "text",
+          id: 'bs4-'+variableDefs.context.name,
+          label: variableDefs.context.name,
+          default: variableDefs.context.value,
+          info: variableDefs.description,
+
+        });
+        break;
+    }
+  }, this);
+
+  // write groups to settings and clean up 
+  for(var name in groups) { 
+    var group = groups[name];
+    bootstrap_theme_settings.settings.push({
+      "type": "header",
+      "content": name
+    });
+    group.forEach(function(groupContext) {
+      if(!groupContext.info || groupContext.info == "" || groupContext.info == "\n") {
+        delete groupContext.info;
+      }
+      bootstrap_theme_settings.settings.push(groupContext);
+    }, this);
+  }
+
+  // save settings to json file
+  fs.writeFileSync('./settings_schema/bootstrap.json', JSON.stringify(bootstrap_theme_settings, null, 2) , 'utf-8');
+
+});
+
+// SHOPIFY_THEME_SETTINGS: Create settings_schema.json
+gulp.task('theme_settings', ['bootstrap_theme_settings'], function () {
+
+  // list of settings files to include, in order of inclusion
+  var settings = [
+    'bootstrap',
+  ];
+
+  return gulp.src('./settings_schema/*.json')
+    .pipe(jsoncombine('settings_schema.json',function(data){
+      // collect the json data and store it in the correct order
+      var data_array = [];
+      for (var i = 0; i < settings.length; i++) {
+        var file = settings[i];
+        data_array.push(data[file]);
+      }
+
+      return new Buffer(JSON.stringify(data_array, null, 2));
+    }))
+    .pipe(gulp.dest('./theme/config/'));
 });

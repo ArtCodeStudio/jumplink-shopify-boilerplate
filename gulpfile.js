bbadd4ab3170c0ea952226e9632332aae9f6c66e
Pascal Garber <pascal@jumplink.eu>
2016-09-21 23:34:18 +0200

    generate _bs4_variables.scss.liquid to set bootstrap variables in scss

--- gulpfile.js
+++ gulpfile.js
@@ -52,18 +52,6 @@ gulp.task('bootstrap_sass_test_build', function () {
     .pipe(gulp.dest('./css'));
 });
 
-// SASS_CONCAT: Pull our scss files together and move them into the themes assets
-gulp.task('sass_concat', function () {
-  var paths = new SassImport('./src/scss/styles.scss');
-  console.log(paths);
-  return gulp.src(paths)
-    .pipe(plumber({
-      errorHandler: onError
-    }))
-    .pipe(concat('styles.scss.liquid'))
-    .pipe(gulp.dest('./theme/assets/'));
-});
-
 // ZIP: Cretae a zipped file of the theme that can be uploaded to Shopify
 gulp.task('zip', function () {
   var theme = [
@@ -79,6 +67,59 @@ gulp.task('zip', function () {
   return gulp.src(theme, {base: "."})
     .pipe(zip('JumpLink-Boilerplate-' + pjson.version + '.zip'))
     .pipe(gulp.dest('./'));
+});
+
+// SASS_CONCAT: Pull our scss files together and move them into the themes assets
+gulp.task('sass_concat', ['bootstrap_theme_settings_scss'], function () {
+  var paths = new SassImport('./src/scss/theme.scss');
+  console.log(paths);
+  return gulp.src(paths)
+    .pipe(plumber({
+      errorHandler: onError
+    }))
+    .pipe(concat('theme.scss.liquid'))
+    .pipe(gulp.dest('./theme/assets/'));
+});
+
+// BOOTSTRAP_THEME_SETTINGS_SCSS: Inject bootstrap theme settings to scss
+gulp.task('bootstrap_theme_settings_scss', ['theme_settings'], function () {
+
+  var bsPkg = require('./bower_components/bootstrap-backward/package.json');
+  var bootstrap = require('./settings_schema/bootstrap.json');
+  var liquidString = "";
+
+
+  bootstrap.settings.forEach(function(setting) {
+    console.log("setting", setting);
+
+    var liquidLine = null;
+    switch (setting.type) {
+      case 'color':
+      case 'number':
+      case 'checkbox':
+      case 'text':
+        liquidLine = "{% if settings.id %} $value: {{settings.id}}; {% endif %}";
+        liquidLine = liquidLine.replace(/id/g, setting.id);
+        liquidLine = liquidLine.replace(/value/g, setting.id.replace('bs4-', ''));
+        break;
+      case 'header':
+       case 'paragraph':
+        liquidLine = "\n{% comment %} content {% endcomment %}".replace('content', setting.content);
+        break;
+      default:
+        console.error("Unknown type: ",setting.type);
+        break;
+    }
+
+    if(liquidLine) {
+      liquidString += liquidLine + "\n";
+    }
+        
+  }, this);
+
+  // save scss file
+  fs.writeFileSync('./src/scss/_bs4_variables.scss.liquid', liquidString , 'utf-8');
+
 });
 
 // BOOTSTRAP_THEME_SETTINGS: Create bootstrap variables for the settings_schema.json
@@ -101,6 +142,11 @@ gulp.task('bootstrap_theme_settings', function () {
   // sort variables to groups
   bootstrap_variables.forEach(function(variableDefs) {
     console.log("variableDefs",variableDefs);
+
+    if(variableDefs.context.scope == "private") {
+      return;
+    }
+
     if(!groups[variableDefs.group[0]]) {
       groups[variableDefs.group[0]] = [];
     }
@@ -140,6 +186,7 @@ gulp.task('bootstrap_theme_settings', function () {
 
         });
         break;
+      case 'text':
       default:
         groups[variableDefs.group[0]].push({
           type: "text",
